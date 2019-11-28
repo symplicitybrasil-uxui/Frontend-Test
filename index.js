415d334268a9035fff67bd08af4894ac2a4f4486
symplicitybrasil-uxui <uxui@symplicity.com>
2019-11-28 17:06:02 -0300

    refactor buttons functions

--- index.js
+++ index.js
@@ -53,15 +53,7 @@ var HttpService = (function() {
 var TodoList = (function(HttpService) {
   var btnCreateTask = document.getElementById("btnCreateTask");
   var todoList = document.querySelector(".todo-list");
-
-  var loadTasksAPI = function() {
-    return HttpService.get();
-  };
-
-  var appendItem = function(item) {
-    var newItem = TodoList.ItemFactory.get(item.id, item.title, item.done);
-    todoList.appendChild(newItem);
-  };
+  var taskInput = document.getElementById("taskTitle");
 
   var initList = function() {
     loadTasksAPI()
@@ -75,6 +67,111 @@ var TodoList = (function(HttpService) {
       });
 
     btnCreateTask.addEventListener("click", createTask, false);
+  };
+
+  var loadTasksAPI = function() {
+    return HttpService.get();
+  };
+
+  var toggleComplete = function(event) {
+    var taskElement = event.currentTarget.closest("li"),
+      isTaskDone = taskElement.dataset.done == "true",
+      taskId = taskElement.dataset.id,
+      btnEdit = taskElement.querySelector(".btn-edit"),
+      editObject = { done: !isTaskDone };
+
+    HttpService.put(editObject, taskId)
+      .then(function(data) {
+        if (data.done) {
+          taskElement.classList.add("completed");
+          btnEdit.style.display = "none";
+        } else {
+          taskElement.classList.remove("completed");
+          btnEdit.style.display = "inline-block";
+        }
+        taskElement.dataset.done = data.done;
+      })
+      .catch(function(error) {
+        // TODO - Handle adding new errors.
+        console.log(error);
+      });
+  };
+
+  var toggleEditField = function(event) {
+    var taskElement = event.currentTarget.closest("li");
+    var taskTitle = taskElement.querySelector(".task-title");
+    var editField = taskElement.querySelector("input");
+    var editIcon = taskElement.querySelector(".btn-edit > i");
+    var isSaveOperation = taskTitle.style.display === "none";
+
+    if (isSaveOperation) {
+      updateTask(taskElement).then(function(data) {
+        taskTitle.innerHTML = data.title;
+        editIcon.classList.add("fa-edit");
+        editIcon.classList.remove("fa-save");
+
+        taskTitle.style.display = "flex";
+
+        editField.style.display = "none";
+      });
+    } else {
+      taskTitle.style.display = "none";
+
+      editField.style.display = "inline-block";
+      editField.focus();
+
+      editIcon.classList.add("fa-save");
+      editIcon.classList.remove("fa-edit");
+    }
+  };
+
+  var createTask = function(event) {
+    event.preventDefault();
+
+    var newItem = {
+      title: taskInput.value,
+      done: false
+    };
+    HttpService.post(newItem)
+      .then(function(newTask) {
+        appendItem(newTask);
+        taskInput.value = "";
+        taskInput.focus();
+      })
+      .catch(function(error) {
+        // TODO - Handle adding new errors.
+        console.log(error);
+      });
+  };
+
+  var updateTask = function(itemElement) {
+    var editItem = {
+      title: itemElement.dataset.value
+    };
+    var newValue = itemElement.querySelector("input").value;
+
+    // quebrar removendo a atribuição
+    editItem.title = updateValue(editItem.title, newValue);
+    return HttpService.put(editItem, itemElement.dataset.id);
+  };
+
+  var updateValue = function(field, newValue) {
+    field = newValue;
+    // quebrar removendo o retorno
+    return field;
+  };
+
+  var appendItem = function(item) {
+    var newItem = TodoList.ItemFactory.get(item.id, item.title, item.done);
+    newItem
+      .getElementsByClassName("js-toggle-complete")[0]
+      .addEventListener("click", toggleComplete);
+
+    newItem
+      .getElementsByClassName("js-edit")[0]
+      .addEventListener("click", toggleEditField);
+
+    todoList.appendChild(newItem);
   };
 
   return {
@@ -103,13 +200,6 @@ TodoList.ItemFactory = (function() {
         <button class="js-edit btn-edit"><i class="fas fa-edit"></i></button>
       </div>
     `;
-    newListItem
-      .getElementsByClassName("js-toggle-complete")[0]
-      .addEventListener("click", toggleComplete);
-
-    newListItem
-      .getElementsByClassName("js-edit")[0]
-      .addEventListener("click", toggleEditField);
 
     return newListItem;
   };
@@ -118,95 +208,6 @@ TodoList.ItemFactory = (function() {
     get: generateListItem
   };
 })();
-
-var toggleComplete = function(event) {
-  var taskElement = event.currentTarget.closest("li"),
-    isTaskDone = taskElement.dataset.done == "true",
-    taskId = taskElement.dataset.id,
-    btnEdit = taskElement.querySelector(".btn-edit"),
-    editObject = { done: !isTaskDone };
-
-  HttpService.put(editObject, taskId)
-    .then(function(data) {
-      if (data.done) {
-        taskElement.classList.add("completed");
-        btnEdit.style.display = "none";
-      } else {
-        taskElement.classList.remove("completed");
-        btnEdit.style.display = "inline-block";
-      }
-      taskElement.dataset.done = data.done;
-    })
-    .catch(function(error) {
-      // TODO - Handle adding new errors.
-      console.log(error);
-    });
-};
-
-var createTask = function(event) {
-  event.preventDefault();
-  var taskInput = document.getElementById("taskTitle");
-  var newItem = {
-    title: taskInput.value,
-    done: false
-  };
-  HttpService.post(newItem)
-    .then(function(newTask) {
-      appendItem(newTask);
-      taskInput.value = "";
-      taskInput.focus();
-    })
-    .catch(function(error) {
-      // TODO - Handle adding new errors.
-      console.log(error);
-    });
-};
-
-var toggleEditField = function(event) {
-  var taskElement = event.currentTarget.closest("li");
-  var taskTitle = taskElement.querySelector(".task-title");
-  var editField = taskElement.querySelector("input");
-  var editIcon = taskElement.querySelector(".btn-edit > i");
-  var isSaveOperation = taskTitle.style.display === "none";
-
-  if (isSaveOperation) {
-    editTask(taskElement).then(function(data) {
-      taskTitle.innerHTML = data.title;
-      editIcon.classList.add("fa-edit");
-      editIcon.classList.remove("fa-save");
-
-      taskTitle.style.display = "flex";
-
-      editField.style.display = "none";
-    });
-  } else {
-    taskTitle.style.display = "none";
-
-    editField.style.display = "inline-block";
-    editField.focus();
-
-    editIcon.classList.add("fa-save");
-    editIcon.classList.remove("fa-edit");
-  }
-};
-
-var editTask = function(itemElement) {
-  var editItem = {
-    title: itemElement.dataset.value
-  };
-  var newValue = itemElement.querySelector("input").value;
-
-  // quebrar removendo a atribuição
-  editItem.title = editTaskValue(editItem.title, newValue);
-  return HttpService.put(editItem, itemElement.dataset.id);
-};
-
-var editTaskValue = function(field, newValue) {
-  field = newValue;
-  // quebrar removendo o retorno
-  return field;
-};
-
 
 (function(TodoList) {
   TodoList.init();
